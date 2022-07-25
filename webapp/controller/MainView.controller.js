754b08f02eacdfabdafd29aebd2176b541153357
ljintriago <ljintriago@hotmail.com>
2022-07-25 21:11:58 +0000

    finalizacion curso sap fiori

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,15 +1,53 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
      */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("seidor.invoice.controller.MainView", {
-            onInit: function () {
 
+            onInit: function () {
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+
+                oJSONModel.loadData("./model/SelectionScreenMenu.json");
+
+                oView.setModel(oJSONModel, "selectionScreen");
+            },
+
+            onFilter: function(oEvent) {
+                const oData = this.getView().getModel("selectionScreen").getData();
+                let filters = [];
+
+                if(oData.ShipName !== "") {
+                    filters.push(new Filter("ShipName", FilterOperator.Contains, oData.ShipName))
+                }
+
+                if(oData.CountryKey !== "") {
+                    filters.push(new Filter("Country", FilterOperator.EQ, oData.CountryKey))
+                }
+
+                const oList = this.getView().byId("invoiceList");
+                const oBinding = oList.getBinding("items");
+                oBinding.filter(filters);
+            },
+
+            onClearFilter: function() {
+                const oModelSelScreen =  this.getView().getModel("selectionScreen");
+                oModelSelScreen.setProperty("/ShipName", "");
+                oModelSelScreen.setProperty("/CountryKey", "");
+                
+                const oList = this.getView().byId("invoiceList");
+                const oBinding = oList.getBinding("items");
+                oBinding.filter([]);
             }
+
         });
     });
